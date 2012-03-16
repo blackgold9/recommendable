eda5381fa9d8d2ccce24f63cf3cb1006191046f0
Stephen Vanterpool <svanter@imdb.com>
2012-03-15 17:33:36 -0700

    Made non class method processor

--- app/workers/recommendable/recommendation_refresher.rb
+++ app/workers/recommendable/recommendation_refresher.rb
@@ -1,8 +1,10 @@
 module Recommendable
   class RecommendationRefresher
-    include Resque::Plugins::UniqueJob
-    @queue = :recommendable
-    
+   # include Resque::Plugins::UniqueJob
+   # @queue = :recommendable
+    def update_recs(user_id)
+      Self.perform(user_id)
+    end
     def self.perform(user_id)
       user = Recommendable.user_class.find(user_id)
       user.send :update_similarities
