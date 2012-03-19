04a4616fbd56a687c11a4c676ced178802060d2f
Stephen Vanterpool <svanter@imdb.com>
2012-03-18 21:32:40 -0700

    recommendable tweak

--- app/workers/recommendable/recommendation_refresher.rb
+++ app/workers/recommendable/recommendation_refresher.rb
@@ -3,8 +3,10 @@ module Recommendable
    # include Resque::Plugins::UniqueJob
    # @queue = :recommendable
     def update_recs(user_id)
-      puts 'updating rects'
-      Self.perform(user_id)
+      puts "Refreshing reccomendations for user"
+      user = Recommendable.user_class.find(user_id)
+      user.send :update_similarities
+      user.send :update_recommendations
     end
     def self.perform(user_id)
       puts "Refreshing reccomendations for user"
